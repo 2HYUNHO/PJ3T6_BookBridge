36557ed914825ac0eb4c20edaa632617e97895b7
2HYUNHO <soda9503@gmail.com>
2024-02-02 14:13:42 +0900

    [Fix] 사진수정 오류 해결

---
+++ BookBridge/BookBridge/Views/Posting/HStackDragTest.swift
@@ -0,0 +1,18 @@
+//
+//  HStackDragTest.swift
+//  BookBridge
+//
+//  Created by jmyeong on 2/1/24.
+//
+
+import SwiftUI
+
+struct HStackDragTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HStackDragTest()
+}
